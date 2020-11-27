7a5e7209b529fc4ce49ecc744b42cea3f2617893
Myroslav Bilohubka <m.bilohubka@protonmail.com>
2020-11-27 16:08:50 +0200

    Added functions InstitutionTypeByCode and RegionNameByCode to the package Catalogs

--- cmd/olimp/catalogs/institutiontype.go
+++ cmd/olimp/catalogs/institutiontype.go
@@ -1,1 +1,17 @@
 package catalogs
+
+type tInstType map[string]string
+
+var instType = tInstType{
+	"1": "Заклад вищої освіти",
+	"2": "Заклад професійної (професійно-технічної) освіти",
+	"3": "Заклад загальної середньої освіти",
+	"5": "Орган управління освітою",
+	"9": "Заклад фахової передвищої освіти",
+}
+
+//InstitutionTypeByCode return type of institution by code
+func InstitutionTypeByCode(code string) (string, bool) {
+	t, ok := instType[code]
+	return t, ok
+}
